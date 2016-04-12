47e14197e19cded4cb6b40e20842f7dd362fc122
Matthew Neal <1002915@student.sae.edu.au>
2016-04-12 11:20:10 +1000

    Upload basics complete

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <?php session_start();?>
 <!DOCTYPE html>
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="Film CMS" content="A place where SAE film students can showcase their Loco-Doco's">
 	<title>Film CMS</title>
 	<meta name="viewport" content="width=device-width" />
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
@@ -9,9 +11,10 @@
 	<link rel="stylesheet" type="text/css" href="css/tatiana_styles.css">
 	<link rel="stylesheet" type="text/css" href="css/alex_styles.css">
 	<link rel="stylesheet" type="text/css" href="css/anna_styles.css">
-	
+	<link rel="stylesheet" type="text/css" href="css/dropzone.css">
 	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/dropzone.js"></script>
 </head>
 
 <body>
@@ -19,7 +22,13 @@
 <div class="site_container">
 	<div class="header_navigation">
 		<a href="index.php"><img src="img/logo.png" class="logo" alt="logo"></a>
-		<?php if (!isset($_SESSION['email'])) { ?>
+
+<?php 
+
+if (!isset($_SESSION['email'])) { 
+
+?>
+
 		<div class="login_header">
 			<form name="login_form_front" id="login" method="post" action="checklogin.php">
   				<input type="text" name='email' placeholder="email" maxlength="50" />
@@ -27,26 +36,22 @@
   				<input class="parallelogram" type='submit' name='Submit' value='login' />
   			</form>
   		</div>
-  		  			<form id="register" action="register.php">
-				<input type="submit" Value="register">
-			</form>
-  		
-		<?php } else echo''; ?>
-		<?php if (isset($_SESSION['email'])) { ?>
+  		<a href="register.php" class="register">Register</a>
 
-		<form class="login_button profile" action="profile.php">
-			<input type="submit" Value="my profile">
-		</form>
+<?php } else echo'';
 
-		<form class="login_button contribute" action="newfilm.php">
-			<input type="submit" Value="contribute">
-		</form>
+if (isset($_SESSION['email'])) { 
 
-		<form class="login_button logout" action="logout.php">
-			<input type="submit" Value="logout">
-		</form>
+?>
+		<a href="profile.php" class="profile">Profile</a>
+		<a href="newfilm.php" class="contribute">Contribute</a>
+		<a href="logout.php" class="logout">Logout</a>
 
-		<?php } else echo''; ?>
+<?php
+
+} else echo''; 
+
+?>
 
 		<div id='search_bar'>
 			<form action="search.php" method="post">
@@ -57,4 +62,5 @@
 
 		</div>
 	</div>
+	
 <div class="site_content">
