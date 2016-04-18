453f32182789d2cca4ba91aa24dd00f425774cca
Matthew Neal <1002915@student.sae.edu.au>
2016-04-19 09:37:44 +1000

    updated footer
    and removed the old login directory.

--- footer.php
+++ footer.php
@@ -1,24 +1,21 @@
 </div>
 </div>
-<footer>
-	<div class="footer">
-		<div class="footer_col_one"> 
-			<a href="index.php">HOME</a> <BR><BR>
-			<a href="login.php"> LOGIN</a> <BR><BR>
-			<a href="register.php"> REGISTER</a> <BR><BR>
-			<a href="search.php"> SEARCH</a> 
-		</div>
-
-	<div class="footer_col_two">
-		 F <BR><BR> LINES TWO <BR><BR> LINES THREE <BR><BR> LINES FOURS
-	</div>
-
-	<div class="footer_col_three"> F <BR><BR> LINES TWO <BR><BR> LINES THREE <BR><BR> LINES FOURS
-	</div>
-
-	<div class="footer_col_four"> ALEX MURPHY <BR><BR> ANNA ESTRELA <BR><BR> MATTHEW NEAL <BR><BR> TATIANA ROGA
-	</div>
 </div>
+<footer>
+        <div class="footer">
+		<div class="footer_col_one"> 
+			<!-- <a href="index.php">HOME</a> 	-->
+		</div>
+	       <div class="footer_col_two">
+		     <!--  <a href="login.php"> LOGIN</a> -->
+	       </div>
+	       <div class="footer_col_three">
+                  contact@email.com  <!--     <a href="register.php"> REGISTER</a> -->
+	       </div>
+	       <div class="footer_col_four">
+                  LocoDoco 2015 <!--      <a href="search.php"> SEARCH</a> -->
+	       </div>
+        </div>
 </footer>
 </body>
 <script>
--- login/resetpassword.php
+++
@@ -1,9 +0,0 @@
-
-<form action="reset.php" method="post" >
-<legend>Reset Password</legend>
-    <input type='hidden' name='submitted' id='submitted' value='1'/>
-    <label for='username' >Your Email*:</label><br/>
-    <input type='text' name='email' id='email' maxlength="50" /><br/>
-    <input type='submit' name='Submit' value='Submit' />
-</form>
-<a href="login.php">back</a>
