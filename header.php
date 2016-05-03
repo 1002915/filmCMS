914f89705cce8e16910d1df45a947133869aeb44
Alex Murphy <a.murphy28@hotmail.com>
2016-05-04 09:10:27 +1000

    title of webpage and favicon

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 
 <!DOCTYPE html>
 <head>
-<title>Film CMS</title>
+<title>Doco Loco</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="Film CMS" content="A place where SAE film students can showcase their Loco-Doco's">
 	<meta name="viewport" content="width=device-width" />
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="css/alex_styles.css">
 	<link rel="stylesheet" type="text/css" href="css/ana_styles.css">
 	<link rel="stylesheet" type="text/css" href="css/dropzone.css">
-
+	<link href="images/favicon.ico" rel="icon" type="image/x-icon" />
 	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 		<script src="js/form-validator/jquery.form-validator.js"></script>
